d7d15c2bffb7ce08be468df5d53fd01e5209d6cb
Chris <chris@nutstree.nl>
2019-02-09 23:08:45 +0100

    first day work

--- src/test/java/nl/nutstree/romannumerals/try20190208/RomanNumerals.java
+++ src/test/java/nl/nutstree/romannumerals/try20190208/RomanNumerals.java
@@ -3,10 +3,46 @@ package nl.nutstree.romannumerals.try20190208;
 import org.apache.commons.lang3.Validate;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RomanNumerals {
-    private static final List<String> legalRomans = List.of("", "I", "V", "X", "L", "C", "D", "M");
+    private enum Roman {
+        I(1),
+        V(5),
+        X(10),
+        L(50),
+        C(100),
+        D(500),
+        M(1000);
+
+        private int numeralValue;
+
+        Roman(int numeralValue) {
+            this.numeralValue = numeralValue;
+        }
+
+        public int toNumeral() {
+            return numeralValue;
+        }
+
+        public static Roman fromValue(String roman) {
+            return Stream.of(Roman.values())
+                    .filter(r -> r.name().equals(roman))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
+
+        static List<Roman> getAll() {
+            return Stream.of(Roman.values())
+                    .collect(Collectors.toList());
+        }
+
+        static List<Roman> getSpecialRomans() {
+            return List.of(V, L, D);
+        }
+    }
+
     private static final String PER_CHAR = "";
 
     private RomanNumerals() {
@@ -14,20 +50,57 @@ public class RomanNumerals {
     }
 
     public static int convert(String roman) {
-        validate(roman);
+        Validate.notBlank(roman);
+        validateSpecialCharsExistOnlyOnce(roman);
+        validateRomanOccurenceCorrect(roman);
+        validateRomanOrdering(roman);
 
-        return roman.length();
+        int total = 0;
+        Roman last = null;
+        List<Roman> romans = toRomanList(roman);
+        for (int i = 0; i < romans.size(); i++) {
+            Roman current = romans.get(i);
+            total += current.numeralValue;
+        }
+        return total;
+
+//        return toRomanList(roman).stream()
+//                .mapToInt(Roman::toNumeral)
+//                .sum();
     }
 
-    private static void validate(String roman) {
-        Validate.notNull(roman);
+    private static void validateRomanOrdering(String romanString) {
+        int last = Integer.MAX_VALUE;
+        for (Roman roman : toRomanList(romanString)) {
+            int current = roman.numeralValue;
+            if (current <= last)
+                last = current;
+            else
+                throw new IllegalArgumentException();
+        }
+    }
 
-        if (isValidRoman(roman))
+    private static void validateRomanOccurenceCorrect(String romanString) {
+        if (!Roman.getAll().stream()
+                .noneMatch(roman -> countRomanInString(romanString, roman) > 3))
             throw new IllegalArgumentException();
     }
 
-    private static boolean isValidRoman(String roman) {
-        return !Stream.of(roman.split(PER_CHAR))
-                .allMatch(legalRomans::contains);
+    private static void validateSpecialCharsExistOnlyOnce(String romanString) {
+        if (!Roman.getSpecialRomans().stream()
+                .noneMatch(roman -> countRomanInString(romanString, roman) > 1))
+            throw new IllegalArgumentException();
+    }
+
+    private static long countRomanInString(String romanString, Roman roman) {
+        return toRomanList(romanString).stream()
+                .filter(roman::equals)
+                .count();
+    }
+
+    private static List<Roman> toRomanList(String roman) {
+        return Stream.of(roman.split(PER_CHAR))
+                .map(Roman::fromValue)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/nl/nutstree/romannumerals/try20190208/RomanNumeralsTest.java
+++ src/test/java/nl/nutstree/romannumerals/try20190208/RomanNumeralsTest.java
@@ -8,17 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class RomanNumeralsTest {
 
     @Test
-    public void empty_0() {
-        assertEquals(RomanNumerals.convert(""), 0);
-    }
-
-    @Test
     public void null_NullException() {
         assertThrows(NullPointerException.class, () -> RomanNumerals.convert(null));
     }
 
     @Test
     public void illegalRoman_illegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert(""));
         assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("P"));
         assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("R"));
         assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("IP"));
@@ -27,18 +23,68 @@ public class RomanNumeralsTest {
     }
 
     @Test
-    public void I_1() {
+    public void singleRoman() {
         assertEquals(RomanNumerals.convert("I"), 1);
+        assertEquals(RomanNumerals.convert("V"), 5);
+        assertEquals(RomanNumerals.convert("X"), 10);
+        assertEquals(RomanNumerals.convert("L"), 50);
+        assertEquals(RomanNumerals.convert("C"), 100);
+        assertEquals(RomanNumerals.convert("D"), 500);
+        assertEquals(RomanNumerals.convert("M"), 1000);
     }
 
     @Test
-    public void II_2() {
+    public void multipleSameRoman() {
         assertEquals(RomanNumerals.convert("II"), 2);
+        assertEquals(RomanNumerals.convert("III"), 3);
+        assertEquals(RomanNumerals.convert("XX"), 20);
+        assertEquals(RomanNumerals.convert("XXX"), 30);
+        assertEquals(RomanNumerals.convert("CC"), 200);
+        assertEquals(RomanNumerals.convert("CCC"), 300);
+        assertEquals(RomanNumerals.convert("MM"), 2000);
+        assertEquals(RomanNumerals.convert("MMM"), 3000);
     }
 
     @Test
-    public void III_3() {
-        assertEquals(RomanNumerals.convert("III"), 3);
+    public void multipleSpecialRomans_illegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("VV"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("VVV"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("LL"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("LLL"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("DD"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("DDD"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("IDD"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("VVX"));
     }
 
+    @Test
+    public void multipleRomansWhoAddUp() {
+        assertEquals(RomanNumerals.convert("VI"), 6);
+        assertEquals(RomanNumerals.convert("XVII"), 17);
+        assertEquals(RomanNumerals.convert("LXVIII"), 68);
+        assertEquals(RomanNumerals.convert("CLXVII"), 167);
+        assertEquals(RomanNumerals.convert("DCLXVI"), 666);
+        assertEquals(RomanNumerals.convert("MMDCCLXXVII"), 2777);
+    }
+
+    @Test
+    public void tooMuchRomans_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("IIII"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("XXXX"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("CCCC"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("MMMM"));
+    }
+
+    @Test
+    public void romansNotCorrectlyOrdered_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("IVII"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("IXCXI"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("MCXIM"));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumerals.convert("CCM"));
+    }
+
+    @Test
+    public void substractRomanValue() {
+        //assertEquals(4, RomanNumerals.convert("IV"));
+    }
 }
